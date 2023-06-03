21ad6d6c1c69de81333e87c5a0a4696208f1afb2
Allison <midastouchhee@gmail.com>
2023-06-03 21:14:09 +0000

    homepage done

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,18 @@
 import React from 'react'
+// import c2 from "../images/c2.jpg"
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <section className='homeContainer home2'>
+       {/* <img src={c2} alt="background"/> */}
+
+      <div className='homeimagewords'>
+        <h1>Home for nfts </h1>
+        <br/>
+          <h1>and creatives</h1>
+        <button>order now</button>
+       </div>
+    </section>
   )
 }
 
--- src/pages/Notification.js
+++ src/pages/Notification.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCheck, faTimesCircle} from '@fortawesome/free-solid-svg-icons';
+import { faCheckCircle, faTimesCircle} from '@fortawesome/free-solid-svg-icons';
 
 
 const Notification = () => {
@@ -9,24 +9,27 @@ const Notification = () => {
       <div className='notificationdropdown'>
 
       <div className="message">
-      <FontAwesomeIcon icon={faCheck}/>
+      <FontAwesomeIcon icon={faCheckCircle}  className='check'/>
         <p>Congratulations, You Just Made A Purchase</p>
 
       </div>
       <div className="message">
-        <FontAwesomeIcon icon={faTimesCircle}/>
+        <FontAwesomeIcon icon={faTimesCircle} className='declined'/>
         <p>Your Card Declined</p>
 
       </div>
       <div className="message">
+        <FontAwesomeIcon icon={faCheckCircle} className='check'/>
         <p>Allison Is Cooking</p>
 
       </div>
       <div className="message">
+      <FontAwesomeIcon icon={faCheckCircle}  className='check'/>
         <p>Don't Miss The Lastest News From Us</p>
 
       </div>
       <div className="message">
+      <FontAwesomeIcon icon={faCheckCircle}  className='check'/>
         <p>Subscribe To Get More Updates Now, Click Here</p>
       </div>
       
